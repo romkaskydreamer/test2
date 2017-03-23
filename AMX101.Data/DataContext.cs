1362b38ff24cf804e5fc65f176bd5c7ecb1af199
romkaskydreamer <st.romka.skydreamer@gmail.com>
2017-03-23 12:55:49 +0200

    4
    4

---
+++ AMX101.Data/DataContext.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using AMX101.Dto.Enitites;
+using AMX101.Dto.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMX101.Data
+{
+    public class DataContext : DbContext
+    {
+        string ConnectionString => string.Format(conn, server, region);
+
+        //ToDO I want to figure out how to get these in app settings 
+        // we have left as is so far until discuss it, just a small refactoring to get it easear to swith servers
+
+        string conn = "Server={0};Database=amx101_{1}_db;User Id=dev;Password=dev;MultipleActiveResultSets=true";
+        //static string conn = "Server={0};Database=amx101_{1}_db;User Id=sa;Password=!QAZ2wsx;MultipleActiveResultSets=true";
+        
+        //string server = @"air.set.local\inst6";
+        string server = @"STUGA-7";
+        // string server = "192.168.1.153";
+        string region; // perhaps we don't need it anymore and have to replace with Config.Region?
+
+        IConfig config;
+        IConfig Config => config ?? (config = new LocalConfig());
+
+        public DataContext(IConfig cnf)
+        {
+            config = cnf;
+            region = Config.Regions[0];
+        }
+
+        public DataContext(string rgn = "")
+        {
+            if (string.IsNullOrEmpty(rgn))
+            {
+                rgn = Config.Regions[0]; // the default region is not gonna change;
+            }
+            if (Config.Regions.Contains(rgn))
+            {
+                region = rgn;
+            }
+        }
+        public DataContext(string rgn, string srv):this(rgn)
+        {
+            if (!string.IsNullOrEmpty(srv))
+            {
+                server = srv;
+            }
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+        }
+
+        public virtual DbSet<PostCode> PostCode {get; set;}
+        public virtual DbSet<ClaimValue> ClaimValue { get; set; }
+        public virtual DbSet<Claim> Claim { get; set; }
+        public virtual DbSet<StaticClaim> StaticClaim { get; set; }
+        public virtual DbSet<Source> Source { get; set; }
+    }
+    /*
+    [Table("PostCode")]
+    public class PostCodeDb
+    {
+
+        public int Id { get; set; }
+        [NotMapped]
+        public int Postcode { get; set; }
+
+        [Column("Postcode")]
+        public string PostcodeC { get; set; }
+
+        public string Suburb { get; set; }
+        public string State { get; set; }
+    }
+    
+     
+             public PostCode(PostCodeInt pci)
+        {
+            Id = pci.Id;
+            Postcode = pci.Postcode.ToString();
+            State = pci.State;
+            Suburb = pci.Suburb;
+        }
+
+     */
+}
---
+++ AMX101.Data/DataRepository.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AMX101.Dto.Enitites;
+using AMX101.Dto.Models;
+
+namespace AMX101.Data
+{
+
+    /// <summary>
+    /// A temporary solution before we do the data access refactoring
+    /// </summary>
+
+    public class DataRepository : IDataRepository
+    {
+        private readonly string server;
+        private readonly string region;
+
+        public DataRepository(string rgn, string srvr = "")
+        {
+            server = srvr;
+            region = rgn;
+        }
+
+        public ICollection<Source> GetSources(string region)
+        {
+            using (var context = new DataContext(region, server))
+            {
+                return context.Source.ToList();
+            }
+        }
+
+        public ICollection<Claim> GetClaims(string region)
+        {
+            using (var context = new DataContext(region, server))
+            {
+                return context.Claim.ToList();
+            }
+        }
+
+        public ICollection<StaticClaim> GetStaticClaims(string region)
+        {
+            using (var context = new DataContext(region, server))
+            {
+                return context.StaticClaim.ToList();
+            }
+        }
+
+        public ICollection<ClaimValue> GetClaimValues(string region)
+        {
+            using (var context = new DataContext(region, server))
+            {
+                return context.ClaimValue.ToList();
+            }
+        }
+
+        public ICollection<ClaimValue> GetClaimValues(string postcode, string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                return context.ClaimValue.Where(x => x.Postcode == postcode).ToList();
+            }
+        }
+
+        public ICollection<StaticClaim> GetStaticClaims(IEnumerable<int> ids, string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                return context.StaticClaim.Where(x => ids.Any(y => y == x.Id)).ToList();
+            }
+        }
+
+        public ICollection<Claim> GetClaims(IEnumerable<int> ids, string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                return context.Claim.Where(x => ids.Any(y => y == x.Id)).ToList();
+            }
+        }
+
+        public ICollection<PostCode> GetPostCodes(string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                return context.PostCode.ToList();
+            }
+        }
+
+        public Claim GetClaim(int id, string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                return context.Claim.FirstOrDefault(x => x.Id == id);
+            }
+        }
+
+        public ICollection<ClaimValue> GetClaimValuesByPrefix(string prefix, string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                return context.ClaimValue.Where(x => x.Postcode.ToString().Substring(0, 2) == prefix).ToList();
+            }
+        }
+
+        public ICollection<PostCode> SearchPostCodes(string query, string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                var lowercaseQuery = query.ToLower();
+                return context.PostCode
+                    .Where(x => x.Postcode.Contains(lowercaseQuery) ||
+                                x.Suburb.ToLower().Contains(lowercaseQuery)).ToList();
+
+            }
+        }
+
+        public bool IsValidPostcode(string query, string region)
+        {
+            using (var context = new DataContext(region))
+            {
+                if (region == "sng")
+                {
+                    return context.ClaimValue.Any(x => x.Postcode.Substring(0, 2).Contains(query));
+                }
+                return context.ClaimValue.Any(x => x.Postcode == query);
+            }
+        }
+
+        public void SaveClaims(IEnumerable<Claim> claims)
+        {
+            using (var context = new DataContext(region))
+            {
+                //first thing we need to do is wipe the old claims as they may have been updated
+                context.ClaimValue.RemoveRange(context.ClaimValue);
+                context.SaveChanges();
+                context.StaticClaim.RemoveRange(context.StaticClaim);
+                context.SaveChanges();
+
+                foreach (var sc in claims)
+                {
+                    context.Claim.Add(sc);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public void SaveClaimValues(IEnumerable<ClaimValue> claimValues)
+        {
+            using (var context = new DataContext(region))
+            {
+                //first thing we need to do is wipe the old claims as they may have been updated
+                context.StaticClaim.RemoveRange(context.StaticClaim);
+                context.SaveChanges();
+
+                foreach (var cv in claimValues)
+                {
+                    context.ClaimValue.Add(cv);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public void SaveStaticClaims(IEnumerable<StaticClaim> staticClaims)
+        {
+            using (var context = new DataContext(region))
+            {                        
+                //first thing we need to do is wipe the old claims as they may have been updated
+                context.StaticClaim.RemoveRange(context.StaticClaim);
+                context.SaveChanges();
+                foreach (var sc in staticClaims)
+                {
+                    context.StaticClaim.Add(sc);
+                }
+
+            }
+        }
+
+        public Source GetOrAddSource(string text)
+        {
+            Source source = null;
+            using (var context = new DataContext())
+            {
+                // add source, in case it's new one
+                if (!string.IsNullOrEmpty(text))
+                {
+                    source = context.Source.FirstOrDefault(s => s.Text == text);
+                    if (source == null)
+                    {
+                        var s = new Source
+                        {
+                            Text = text
+                        };
+                        context.Source.Add(s);
+                        context.SaveChanges();
+
+                        source = s;
+                    }
+                }
+            }
+            return source;
+        }
+    }
+}
